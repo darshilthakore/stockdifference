0ab42034b76f4b49940d30ae4ed5132511eabcc6
Darshil Thakore <darshilthakore@gmail.com>
2020-06-18 19:47:11 +0530

    new redis url

--- worker.py
+++ worker.py
@@ -5,13 +5,10 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL')
+redis_url = os.getenv('REDISTOGO_URL', 'redis://redistogo:1da21a3cf6818041c5b94973b9fdde09@pike.redistogo.com:11316/
+')
 
-urlparse.uses_netloc.append('redis')
-url = urlparse.urlparse(redis_url)
-conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
-
-
+conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
