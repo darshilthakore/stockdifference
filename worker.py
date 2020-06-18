5ad7e6f7c9c1b1bafd4c41aca890bb02af49760d
Darshil Thakore <darshilthakore@gmail.com>
2020-06-18 19:50:16 +0530

    new redis url 1

--- worker.py
+++ worker.py
@@ -5,8 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://redistogo:1da21a3cf6818041c5b94973b9fdde09@pike.redistogo.com:11316/
-')
+redis_url = os.getenv('REDISTOGO_URL', 'redis://redistogo:1da21a3cf6818041c5b94973b9fdde09@pike.redistogo.com:11316/')
 
 conn = redis.from_url(redis_url)
 
